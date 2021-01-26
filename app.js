bea999b68dbbd1d1fdec1d4ad49f297c51667717
ginestah <68362184+ginestah@users.noreply.github.com>
2021-01-26 17:01:30 -0500

    end of day commit

--- app.js
+++ app.js
@@ -16,14 +16,6 @@ async function fetchData(date) {
 
 
 //function to append content to the DOM
-// function showNasaPhoto(data) {
-//   let nasaPhoto = `
-// <img src='${data.hdurl}'>
-// `
-//   let photoContainer = document.querySelector('#photo-container')
-//   photoContainer.insertAdjacentHTML('beforeend', nasaPhoto)
-
-// }
 
 function showNasaPhoto(data) {
   if (data.copyright == undefined) {
